d4abf9f229ad985bc33efab022578e42444073cd
adamheincz <heinczadam@gmail.com>
2025-03-24 14:43:16 +0100

    Made some adjustments on the admin UI.

--- frontend/src/components/CurrentFolder.jsx
+++ frontend/src/components/CurrentFolder.jsx
@@ -24,8 +24,29 @@ export default function CurrentFolder({ selectedFolder, onSelectFolder, directio
         handleFile(fileUploaded);
     }
 
+    console.log(hiddenFileInput)
+
     return (
         <div className='flex flex-col flex-1 min-h-0 relative overflow-hidden'>
+            <div className='flex gap-4 w-full bg-zinc-950 z-10'>
+                <button
+                    className='bg-zinc-900 text-white px-4 py-2 rounded-lg mb-3'>
+                    Új mappa +
+                </button>
+                <div>
+                    <button
+                        className='bg-zinc-900 text-white px-4 py-2 rounded-lg mb-3'
+                        type="button" onClick={handleClick}>
+                        Kép feltöltés
+                    </button>
+                    <input
+                        ref={hiddenFileInput}
+                        type="file"
+                        style={{ display: 'none' }}
+                        onChange={handleChange}
+                    />
+                </div>
+            </div>
             <AnimatePresence mode='popLayout' custom={direction}>
                 <motion.div
                     variants={variants}
@@ -43,10 +64,6 @@ export default function CurrentFolder({ selectedFolder, onSelectFolder, directio
                                 Mappák
                             </h3>
                             <div>
-                                <button
-                                    className='bg-zinc-900 text-white px-4 py-2 rounded-lg mb-3'>
-                                    Új mappa +
-                                </button>
                                 {selectedFolder.folders &&
                                     <ul className="text-white text-2xl">
                                         {selectedFolder.folders.map(folder =>
@@ -75,21 +92,6 @@ export default function CurrentFolder({ selectedFolder, onSelectFolder, directio
                             {selectedFolder.images &&
                                 <ul
                                     className="flex flex-wrap gap-6 shrink basis-auto min-h-0 overflow-y-scroll pr-1">
-                                    <li>
-                                        <button
-                                            className='bg-zinc-900 w-36 h-full rounded-lg material-icons'
-                                            type="button" onClick={handleClick}>
-                                            <span className='text-4xl'>
-                                                add_photo_alternate
-                                            </span>
-                                        </button>
-                                        <input
-                                            ref={hiddenFileInput}
-                                            type="file"
-                                            style={{ display: 'none' }}
-                                            onChange={handleChange}
-                                        />
-                                    </li>
                                     {selectedFolder.images.length &&
                                         selectedFolder.images.map((image, index) => {
                                             return (
